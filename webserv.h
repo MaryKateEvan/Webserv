757c2a71f2e0402d84207063c97e53ca005cd65f
Eugen <118663096+itseugen@users.noreply.github.com>
2024-09-12 15:47:08 +0000

    added libraries

---
+++ webserv.h
@@ -0,0 +1,14 @@
+#ifndef WEBSERV_H
+# define WEBSERV_H
+
+# include <iostream>
+# include <string>
+# include <vector>
+# include <deque>
+# include <algorithm>
+# include <limits>
+# include <sys/time.h>
+# include <climits>
+# include <sstream>
+
+#endif /*WEBSERV_H*/
