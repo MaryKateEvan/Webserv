386e6d19792b73f932ce75e874ce61c791394e15
Eugen <118663096+itseugen@users.noreply.github.com>
2024-09-16 14:53:14 +0000

    first layout with poll

--- main.cpp
+++ main.cpp
@@ -2,85 +2,119 @@
 
 int main()
 {
-    int fd_server;
+	int	fd_server;
 
-    // Test for responses!
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(0, 1);
+	fd_server = socket(AF_INET, SOCK_STREAM, 0);
+	// AF_INET for IPv4 (http), SOCK_STREAM as a standart byte based sequenced stream (for tcp), 0 as no specific protocol is used
 
-    fd_server = socket(AF_INET, SOCK_STREAM, 0);
-    // AF_INET for IPv4 (http), SOCK_STREAM as a standart byte based sequenced stream (for tcp), 0 as no specific protocol is used
-    
-    if (fd_server == -1)
-    {
-        std::cerr << "Socket creation failed" << std::endl;
-        return (1);
-    }
-    
-    struct sockaddr_in  address;
-    
-    address.sin_family = AF_INET;
-    //always AF_INET (otherwise error returned)
-    
-    address.sin_port = htons(PORT);
-    //port in network byte order (htonl to confert integer to network Byte order), PORTS under 1024 may only bind if privileged
-    
-    address.sin_addr.s_addr = INADDR_ANY;
-    //IP host address (stored in network byte order), INADDR_ANY to assign to 0.0.0.0 = any address, use inet_aton() to convert IP Address into network byte order
-    
-    if (bind(fd_server, (struct sockaddr *)&address, sizeof(address)) != 0)
-    {
-    //bind to assign an address to the file descriptor
-        std::cerr << "Binding to socket failed" << std::endl;
-        close(fd_server);
-        return (1);
-    }
+	if (fd_server == -1)
+	{
+		std::cerr << "Socket creation failed" << std::endl;
+		return (1);
+	}
 
-    if (listen(fd_server ,SOMAXCONN) != 0)
-    {
-    //marks socket as a passive socket that will be used to accept connections using accept
-    //takes the fd and a int as a backlog (how many request can be queued)
-        std::cerr << "Setting file descriptor to listen failed" << std::endl;
-        close(fd_server);
-        return (1);
-    }
-    std::cout << "Server is now listening on port " << PORT << std::endl;
-    int addr_len = sizeof(address);
-    while (true)
-    {
-        int fd_new_socket = accept(fd_server, (struct sockaddr *)&address, (socklen_t *)&addr_len);
-        //accepting the first connection req from the queue. Creates a new connected socket (the old socket is unaffected)
+	struct sockaddr_in  address;
 
-        if (fd_new_socket == -1)
-        {
-            std::cerr << "Accept failed" << std::endl;
-            close(fd_server);
-            return (1);
-        }
+	address.sin_family = AF_INET;
+	//always AF_INET (otherwise error returned)
 
-        char buffer[2048] = {0};
-        read(fd_new_socket, buffer, 1024);
-        //just a temporary thing, use something like gnl in the future + a parser to handle requests correctly
-        std::cout << "Received request: " << std::endl << buffer << std::endl;
+	address.sin_port = htons(PORT);
+	//port in network byte order (htonl to confert integer to network Byte order), PORTS under 1024 may only bind if privileged
 
-        if (dist(gen) == 0)
-        {
-            const char *response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nHello World this is from Webserv!";
-            send(fd_new_socket, response, strlen(response), 0);
-        }
-        else
-        {
-            const char *response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nHello, World this is the other response from Webserv!";
-            send(fd_new_socket, response, strlen(response), 0);
-            //same as write if the FLAGS are set to 0 like rn
-        }
-        //basic hello world page, doesnt matter what the actuall request is for now
+	address.sin_addr.s_addr = INADDR_ANY;
+	//IP host address (stored in network byte order), INADDR_ANY to assign to 0.0.0.0 = any address, use inet_aton() to convert IP Address into network byte order
 
+	if (bind(fd_server, (struct sockaddr *)&address, sizeof(address)) != 0)
+	{
+	//bind to assign an address to the file descriptor
+		std::cerr << "Binding to socket failed" << std::endl;
+		close(fd_server);
+		return (1);
+	}
 
-        close(fd_new_socket);
-    }
-    close(fd_server);
+	if (listen(fd_server ,SOMAXCONN) != 0)
+	{
+	//marks socket as a passive socket that will be used to accept connections using accept
+	//takes the fd and a int as a backlog (how many request can be queued)
+		std::cerr << "Setting file descriptor to listen failed" << std::endl;
+		close(fd_server);
+		return (1);
+	}
+	std::cout << "Server is now listening on port " << PORT << std::endl;
 
-    return (0);
+	struct pollfd	fds[MAX_CLIENTS];
+	int				nfds = 1;
+
+	fds[0].fd = fd_server;
+	fds[0].events = POLLIN;
+
+	for (int i = 1; i < MAX_CLIENTS; i++)
+	{
+		fds[i].fd = -1;
+	}
+
+	//Server Loop
+	while (true)
+	{
+		int	poll_count = poll(fds, nfds, -1);
+
+		if (poll_count == -1)
+		{
+			std::cerr << "poll() failed" << std::endl;
+			break ;
+		}
+
+		if (fds[0].revents & POLLIN)
+		{
+			socklen_t	addr_len = sizeof(address);
+			int			fd_new_socket = accept(fd_server, (struct sockaddr *)&address, &addr_len);
+
+			if (fd_new_socket == -1)
+			{
+				std::cerr << "Accept failed" << std::endl;
+				continue ;
+			}
+
+			for (int i = 1; i < MAX_CLIENTS; i++)
+			{
+				if (fds[i].fd == -1)
+				{
+					fds[i].fd = fd_new_socket;
+					fds[i].events = POLLIN;
+					nfds = std::max(nfds, i + 1);
+					std::cout << "New client connected on fd " << fd_new_socket << std::endl;
+					break;
+				}
+			}
+		}
+
+		for (int i = 1; i < nfds; i++)
+		{
+			if (fds[i].fd != -1 && (fds[i].revents & POLLIN))
+			{
+				char	buffer[4096] = {0};
+				int		bytes_read = read(fds[i].fd, buffer, 4096);
+
+				if (bytes_read <= 0)
+				{
+					std::cout << "Client on fd " << fds[i].fd << " disconnected" << std::endl;
+					close(fds[i].fd);
+					fds[i].fd = -1;
+				}
+				else
+				{
+					std::cout << "Received request from client on fd " << fds[i].fd << std::endl;
+
+					const char *response = "HTTP/1.1 200 OK\nContent-Type: text/html\n\nHello World this is from Webserv!";
+					if (send(fds[i].fd, response, strlen(response), 0) == -1)
+					{
+						std::cerr << "Send failed on fd " << fds[i].fd << std::endl;
+					}
+				}
+			}
+		}
+	}
+	close(fd_server);
+
+	return (0);
 }
