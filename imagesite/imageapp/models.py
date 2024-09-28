d9b33dcc8435105965e0f9197c5d13ca670b3d22
Eugen <118663096+itseugen@users.noreply.github.com>
2024-09-28 11:36:46 +0000

    Gone

--- imagesite/imageapp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Image(models.Model):
-    image = models.ImageField(upload_to='usrimg/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
--- imagesite/imageapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.image_gallery, name='image_gallery'),
-    path('upload/', views.upload_image, name='upload_image'),
-    path('delete/<int:image_id>/', views.delete_image, name='delete_image'),
-]
