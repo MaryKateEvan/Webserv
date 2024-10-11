054a84f4c844cf59f40a720172ef7c45d7a876eb
Eugen <118663096+itseugen@users.noreply.github.com>
2024-10-11 16:23:31 +0000

    Fixed small issue with directory listing

--- server/Server.cpp
+++ server/Server.cpp
@@ -183,7 +183,7 @@ int	Server::process_get(const Request& req)
 
 	if (std::filesystem::is_directory(file_path))
 	{
-		if (file_path != map_to_directory("/" + _data_dir + "/"))
+		if (file_path != map_to_directory("/" + _data_dir + "/") && file_path != map_to_directory("/" + _data_dir))
 		{
 			// std::cout << "File Path: " << file_path << std::endl;
 			// std::cout << "Data Path: " <<  map_to_directory("/" + _data_dir) << std::endl;
