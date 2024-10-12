3d86bccbbcabb47c3ef951a4e5783eaf038b0fe8
Eugen <118663096+itseugen@users.noreply.github.com>
2024-10-12 15:55:46 +0000

    Added my webpage as a submodule

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main()
 		std::signal(SIGINT, signal_handler);
 		// Server	server1("A little webserver", PORT, "0.0.0.0", "index.html", "usrimg", "www");
 		// Server	server1("A Instagram Knockoff", PORT, "0.0.0.0", "index.html", "images", "image_website");
-		Server	server1("A Instagram Knockoffnbr2", PORT, "0.0.0.0", "index.html", "usrimg", "myWebsiteGithub");
+		Server	server1("A Instagram Knockoffnbr2", PORT, "0.0.0.0", "index.html", "usrimg", "www_image_webpage");
 		// Set response Method is outdated since the server is using the file system in dir /www/
 		server1.setResponse("HTTP/1.1 200 OK\nContent-Type: text/html\n\nA surprise to be sure, but a welcome one!\n");
 
