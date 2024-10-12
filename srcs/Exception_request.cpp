3f8b36dc88f8853e742373c5e4642df0ad9b53a2
MaryKateEvan <marykate_evan@outlook.com.gr>
2024-10-13 00:39:31 +0200

    still fixing the structure. i think now i have all the files combined. still need to reach compilation

--- srcs/Exception_request.cpp
+++
@@ -1,4 +0,0 @@
-#include "../includes/Exceptions.hpp"
-
-NoMethodFoundException::NoMethodFoundException(const std::string& request)
-	: BaseException("No Method (GET/POST/DELETE) could be found in the request: " + request) {}
