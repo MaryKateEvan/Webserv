95c41ab5dc83f423735e8409e24ff0e5632d9372
Eugen <118663096+itseugen@users.noreply.github.com>
2024-10-28 21:54:27 +0000

    Logger works now

--- log/Logger.cpp
+++ log/Logger.cpp
@@ -6,8 +6,13 @@
 
 Logger::Logger()
 {
+	const std::filesystem::path&	folder1 = "log/server";
+	const std::filesystem::path&	folder2 = "log/error";
+	if (!std::filesystem::exists(folder1))
+		std::filesystem::create_directories(folder1);
+	if (!std::filesystem::exists(folder2))
+		std::filesystem::create_directories(folder2);
 	std::string	time = get_time();
-	const std::filesystem::path&	folder1 = "/log/server";
 	if (std::filesystem::exists(folder1) && std::filesystem::is_directory(folder1))
 	{
 		for (const auto& entry : std::filesystem::directory_iterator(folder1))
@@ -18,9 +23,18 @@ Logger::Logger()
 			}
 		}
 	}
-	delete_oldest_file("/log/error", 5);
-	_server_log.open("/log/server/" + time + "server.log");
-	_error_log.open("/log/error/" + time + "server.error.log");
+	delete_oldest_file("log/error", 5);
+	_server_log.open(("log/server/" + time + "server.log"));
+	// _server_log.open("log/server/test");
+	if (!_server_log.is_open())
+	{
+		throw std::ios_base::failure("Failed to open server log file");
+	}
+	_error_log.open("log/error/" + time + "server.error.log");
+	if (!_error_log.is_open())
+	{
+		throw std::ios_base::failure("Failed to open error log file");
+	}
 }
 
 Logger::~Logger()
@@ -45,11 +59,14 @@ void	Logger::delete_oldest_file(const std::filesystem::path& folder, size_t max_
 		{
 			if (std::filesystem::is_regular_file(entry.status()) && entry.path().extension() == ".log")
 			{
-				files.emplace_back(entry.path(), std::filesystem::last_write_time(entry));
+				auto	ftime = std::filesystem::last_write_time(entry);
+				auto	cftime = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
+					ftime - std::filesystem::file_time_type::clock::now() + std::chrono::system_clock::now());
+				files.emplace_back(entry.path(), cftime);
 			}
 		}
 	}
-	if (files.size() > max_files)
+	if (files.size() >= max_files)
 	{
 		std::sort(files.begin(), files.end(), [](const auto& a, const auto& b) {
 			return (a.second < b.second);
@@ -64,25 +81,33 @@ void	Logger::delete_oldest_file(const std::filesystem::path& folder, size_t max_
  * @brief Adds a log to the logfile
  * @param server_name the name of the server, empty if the error is general
  * @param message the message, in the standart format, has to be in the Common Log Format for the server log
- * @param level 0 for DEBUG, 1 for INFO, 2 for WARNING, 3 for ERROR, ERROR only for FATAL error meaning server failure
+ * @param level 0 for DEBUG, 1 for CLF, 2 for INFO to error log, 3 for WARNING, 4 for ERROR, ERROR only for FATAL error meaning server failure
  */
 void	Logger::log(const std::string& server_name, const std::string& message, int level)
 {
 	std::string	time = get_time();
+	std::string	msg = message;
+	if (!server_name.empty())
+	{
+		msg = server_name + ":\t" + message;
+	}
 	switch (level)
 	{
 		case 1:
-			_server_log << message << std::endl;
+			_server_log << msg << std::endl;
 			break;
 		case 0:
-			_error_log << time << " [DEBUG]\t" << message << std::endl;
+			_error_log << time << " [DEBUG]\t" << msg << std::endl;
 			break;
 		case 2:
-			_error_log << time << " [WARNING\t]" << message << std::endl;
+			_error_log << time << " [INFO]\t" << msg << std::endl;
 			break;
 		case 3:
-			_error_log << time << " [ERROR\t]" << message << std::endl;
-			_server_log << time << " [ERROR\t]" << message << std::endl;
+			_error_log << time << " [WARNING]\t" << msg << std::endl;
+			break;
+		case 4:
+			_error_log << time << " [ERROR]\t" << msg << std::endl;
+			_server_log << time << " [ERROR]\t" << msg << std::endl;
 			break;
 		default:
 			break;
@@ -95,7 +120,7 @@ std::string	Logger::get_time()
 	std::time_t	now = std::time(NULL);
 	std::tm local_tm = *std::localtime(&now);
 	char	buffer[100];
-	std::string	format = "%d/%b/%Y:%H:%M:%S";
+	std::string	format = "[%d-%m-%Y:%H:%M:%S]";
 	if (std::strftime(buffer, sizeof(buffer), format.c_str(), &local_tm))
 	{
 		time = std::string(buffer);
