1c83bbb36f1034f614873454d21f8432f6f292c4
Eugen <118663096+itseugen@users.noreply.github.com>
2024-10-30 14:20:39 +0000

    Added script Co-authored-by: Mary Kate Evangelidou <MaryKateEvan@users.noreply.github.com>

---
+++ www/cgi-bin/hello_world.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+print("HTTP/1.1 200 OK\r\n")
+print("Content-Type: text/html\r\n")
+print("Content-Lenght: 64\r\n\r\n")
+print("<html><body>")
+print("<h1>Hello, World from Python CGI!</h1>")
+print("</body></html>")
