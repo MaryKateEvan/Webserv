7da582dcec0df9abb6fd431997b19d67bae0d823
Eugen <118663096+itseugen@users.noreply.github.com>
2024-10-31 12:52:26 +0000

    Fixed filepath issue I created

--- www/cgi-bin/hello_world.py
+++ www/cgi-bin/hello_world.py
@@ -4,6 +4,7 @@ import time
 print("HTTP/1.1 200 OK\r")
 print("Content-Type: text/html\r")
 print("Content-Length: 64\r\n\r")
+time.sleep(15)
 print("<html><body>")
 print("<h1>Hello, World from Python CGI!</h1>")
 print("</body></html>")
