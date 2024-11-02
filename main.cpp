36781e4e53fe14f4b37a52e8bae6c8e7930ea81b
MaryKateEvan <marykate_evan@outlook.com.gr>
2024-11-02 20:00:59 +0200

    added an informative message for the user in the beginning to inform about the log directory. if no args main calls the default.conf. And i deleted the comments of the previous servers initialization in the main

--- main.cpp
+++ main.cpp
@@ -17,12 +17,15 @@ void signalHandler(int signum)
 
 int main(int argc, char **argv)
 {
-	std::cout << GREEN("🚀 Launching Webserv ... 🚀") << std::endl;
 	if (argc > 2)
 	{
-		std::cout << ERROR("Expected usage: ") << UNDERLINE("./webserv") << " or " << UNDERLINE("./webserv <path_to_config_file>") << std::endl;
+		std::cout << ERROR("use either ") << UNDERLINE("./webserv") << " or " 
+				<< UNDERLINE("./webserv <path_to_config_file>") << std::endl;
 		return 0;
 	}
+	std::cout << GREEN("\n🚀 Webserv is up and running ... 🚀\n") << std::endl;
+	std::cout << YELLOW(BOLD("💡 TIP: ")) << "For server's " << BOLD("output") << " and/or " 
+			<< BOLD("errors") << " check the " << UNDERLINE("Log directory") << ". 📁 🔍" << std::endl;
 
 	std::vector<ServerData> server_vec;
 	try
@@ -30,7 +33,7 @@ int main(int argc, char **argv)
 		if (argc == 2)
 			server_vec = read_config_file(argv[1]);
 		else
-			server_vec = read_config_file("config_files/example.conf");
+			server_vec = read_config_file("config_files/default.conf");
 	}
 	catch(const std::exception& e)
 	{
@@ -49,13 +52,11 @@ int main(int argc, char **argv)
 			socket_manager.add_server(server.port_to_listen, std::make_unique<Server>(server.server_name, server.port_to_listen, "0.0.0.0", server.index_file,
 			"usrimg", server.root, server.directory_listing, server.keepalive_timeout, server.send_timeout, server.max_request_size, server.locations));
 		}
-		// socket_manager.add_server(8080, std::make_unique<Server>("Instalight", 8080, "0.0.0.0", "index.html", "usrimg", "www_image_webpage", true, 0, 0, 3000000));
-		// socket_manager.add_server(8081, std::make_unique<Server>("A little webserver", 8081, "0.0.0.0", "index.html", "usrimg", "www", true, 0, 0, 3000000));
 		socket_manager.handle_requests();
 	}
 	catch (const std::exception& e)
 	{
-			Logger::getInstance().log("", e.what(), 4);
+		Logger::getInstance().log("", e.what(), 4);
 	}
 	return (0);
 }
